a0c3809deca585893f99ca6980e62ad9a20246e8
João Victor Medeiros dos Santos <153537038+Herbaszl@users.noreply.github.com>
2024-09-24 14:04:58 -0300

    Add files via upload

---
+++ exercicio_typescript.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(valor1, valor2) {
+    return valor1 * valor2;
+}
+function DizOla(nome) {
+    return `Olá ${nome}, seja bem vindo ao curso Front-End!`;
+}
+console.log(multiplicar(28, 30));
+console.log(DizOla("Ornitorrinco Espacial"));
---
+++ exercicio_typescript.ts
@@ -0,0 +1,10 @@
+function multiplicar(valor1: number, valor2: number): number{
+    return valor1 * valor2;
+}
+
+function DizOla(nome: string):string{
+    return `Olá ${nome}, seja bem vindo ao curso Front-End!`;
+}
+
+console.log(multiplicar(28, 30));
+console.log(DizOla("Ornitorrinco Espacial"));
